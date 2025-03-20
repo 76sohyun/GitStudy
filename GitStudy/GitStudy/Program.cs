44b7bdd6b1ce6c1ad13338b2fb40cefd924670e5
76sohyun <conut1119@gmail.com>
2025-03-20 12:45:12 +0900

    Feat Add New Task

--- GitStudy/GitStudy/Program.cs
+++ GitStudy/GitStudy/Program.cs
@@ -6,8 +6,10 @@
         {
             Console.Write("이름을 입력해주세요 : ");
             string input = Console.ReadLine();
+            Console.Write("나이를 입력해주세요 : ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"이름은 {input}이고 나이는 {age}살 입니다.");
 
-            Console.WriteLine($"입력하신 이름은 {input} 입니다.");
         }
     }
 }
